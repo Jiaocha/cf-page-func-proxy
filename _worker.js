fff1441c36e6b041286dd1157f31399e16b79a95
Jiaocha <49628618+Jiaocha@users.noreply.github.com>
2025-03-11 11:17:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,12 +1,12 @@
-export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="example.com";
-        let new_request=new Request(url,request);
+export 默认 {
+    async fetch(request， env) {
+      let url = new URL(request。url);
+      if (url。pathname。startsWith('/')) {
+        url。hostname="cf-shsus.256709394.xyz";
+        let new_request=new Request(url，request);
         return fetch(new_request);
       }
       // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+      return env。ASSETS。fetch(request);
     }
   };
